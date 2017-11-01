d74494a156b109e7a08afe0062122a43b18ba646
Paul Tonning <vvcinstructor@gmail.com>
2017-11-01 16:36:38 -0700

    update

--- sub4/main4.asm
+++ sub4/main4.asm
@@ -1,19 +1,7 @@
-
-
 ;
 ; file: main4.asm
 ; Multi-module subprogram example program
 ;
-; To create executable:
-; Using djgpp:
-; nasm -f coff sub4.asm
-; nasm -f coff main4.asm
-; gcc -o sub4 sub4.o main4.o driver.c asm_io.o
-;
-; Using Borland C/C++
-; nasm -f obj sub4.asm
-; nasm -f obj main4.asm
-; bcc32 sub4.obj main4.asm driver.c asm_io.obj
 
 %include "asm_io.inc"
 
